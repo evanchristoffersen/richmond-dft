cdc2573f627d21d926904cbccd5aed337111c472
Evan Christoffersen <34754998+evanchristoffersen@users.noreply.github.com>
2021-06-30 12:16:01 -0700

    Trying to commit the directory and shell script contents of the 'generate_' bash scripts to the github remote repository

---
+++ generate_inp_pbs_files/generate_optG.sh
@@ -0,0 +1,171 @@
+#!/usr/bin/env bash
+
+# Non-posix compliant tools used in this script:
+# gnu sed (gsed)
+# find
+# wc
+# xargs
+
+# abort on nonzero exit status
+set -o errexit 
+# abort on unbound variable
+set -o nounset
+# dont hide errors within pipes
+set -o pipefail
+
+[ -d "geometries/unoptimized/" ] || printf "\nThe ./geometries/unoptimized/ \
+directory doesnt exist. \n\n" # checks to see if the directory containing
+# the unoptimized geometries of the conformers exists - exits on return 1
+
+cd "geometries/unoptimized/" # enters that directory
+
+ls -l *.xyz &>/dev/null || printf "\nNo .xyz files detected in the \
+./geometries/unoptimized/ directory. \n\n" # lists all files ending in .xyz 
+# and exits on a return 1
+
+numxyz=$(ls -l *.xyz 2>/dev/null | wc -l | xargs) # lists all files ending
+# in .xyz and pipes it into wc which counts the  number of files in the ls 
+# output and pipes to xargs which removes the leading spaces from wc output
+
+echo
+echo "${numxyz}" conformers detected in ./geometries/unoptimized/
+
+cd ../.. # returns to regular working directory
+
+# Exits script if template files don't exist or path to them can't be found
+if [ ! -f "template_inppbs/template_opt.inp" ] || [ ! -f \
+"template_inppbs/template_opt.pbs" ]; then
+    echo
+    echo The filepath is broken or file is missing for one or both of:
+    echo
+	echo ./template_inppbs/template_opt.inp
+    echo ./template_inppbs/template_opt.pbs 
+	echo
+	exit
+fi
+
+[ -d "conformer_files/" ] || mkdir "conformer_files/" # generates the directory
+# to hold all of the conformer computational files if it doesn't already exist
+
+echo
+echo To generate opt.inp and opt.pbs files for talapas, 
+echo choose a conformer library:
+echo
+echo NOTE: It is not recommended to generate opt.pbs and opt.inp files for
+echo every conformer unless there are only around 120 conformers total.
+echo
+
+i=0
+unset options # clears variable for use
+while IFS= read -r -d $'\0' f; do # reads the input from "find" - null 
+#delimited (-d $'\0')
+        options[i++]="${f}" # saves input from "find" to options variable
+done < <(find ./tmpfiles \( ! -regex '.*/\..*' \) -maxdepth 1 -type f -name \
+"reduced*.csv" -print0 ) # searches /dir/ and no deeper (maxdepth = 1) for
+# files with the reduced*.csv name and prints them delimited by the null 
+# character (-print0)
+# the regex bit takes allows find to ignore hidden files and directories
+
+select opt in "${options[@]}" \
+"Use all ${numxyz} conformers (not recommended)" "Exit script"; do
+# lookup select and case tutorials for explanation - bottom line: this
+# creates a simple user menu
+	case $opt in 
+		*.csv) 
+			echo
+			echo You selected the reduced conformer library "${opt}" 
+			library="${opt}" # saves selected library file for use later
+			exitcode="reduced" # provides a key for upcoming if then statement
+			echo 
+			break
+			;; 
+		"Use all ${numxyz} conformers (not recommended)") 
+			echo
+			echo You chose to generate opt.inp and opt.pbs files for every \
+            conformer.
+			exitcode="all" # provides a key for upcoming if then statement
+			echo 
+			break 
+			;; 
+		"Exit script") 
+			echo
+			echo Exiting script. 
+			echo 
+			exit 
+			;; 
+		*) # if user attempts to enter something other than one of the 
+        # available options, they're prompted to try again
+			echo
+			echo "This is not an option. Try again."
+			echo 
+			;; 
+	esac # "case" backwards (like if & fi or do & done)
+done
+
+# makes directories for each of the conformers for gaussian calculations
+if [ ${exitcode} = "reduced" ]; then
+	for conf in $(<${library}); do
+		conf="${conf##*/}"
+		conf="${conf%.*}"
+		[ -d "conformer_files/${conf}/" ] || mkdir "conformer_files/${conf}/" 
+	done
+elif [ ${exitcode} = "all" ]; then
+	for conf in geometries/unoptimized/*.xyz; do
+		conf="${conf##*/}"
+		conf="${conf%.*}"
+		[ -d "conformer_files/${conf}/" ] || mkdir "conformer_files/${conf}/"
+	done
+fi
+
+# Saves template file locations to variables
+inptemplate="template_inppbs/template_opt.inp"
+pbstemplate="template_inppbs/template_opt.pbs"
+
+# opt.inp and opt.pbs file creation
+echo Generating opt.inp and opt.pbs files for each conformer...
+echo
+for confdir in conformer_files/*; do
+	confdir="${confdir##*files/}"
+	confdir="${confdir%/*}"
+	if [ ! -f "conformer_files/${confdir}/${confdir}_opt.inp" ] && \
+    [ ! -f "conformer_files/${confdir}/${confdir}_opt.pbs" ]; then
+		if [ ! -f "geometries/unoptimized/${confdir}.xyz" ]; then
+			echo "ERROR: Missing file ./geometries/unoptimized/${confdir}.xyz"
+		else
+			cp -nv "${inptemplate}" \
+            "conformer_files/${confdir}/${confdir}_opt.inp"
+
+			cp -nv "${pbstemplate}" \
+            "conformer_files/${confdir}/${confdir}_opt.pbs"
+
+			cat "geometries/unoptimized/${confdir}.xyz" >> \
+            "conformer_files/${confdir}/${confdir}_opt.inp" # entire .xyz file
+            # is written to opt.inp
+
+			gsed -i '10,14d' "conformer_files/${confdir}/${confdir}_opt.inp" 
+            # removes lines 10 - 14
+
+			echo -e '\n \n' >> "conformer_files/${confdir}/${confdir}_opt.inp"
+            # two new lines are added to the end of opt.inp
+            # (gaussian requirement)
+
+			gsed -i "s/PLACEHOLDER/${confdir}/g" \
+            "conformer_files/${confdir}/${confdir}_opt.inp" # replaces all
+            # instances of "PLACEHOLDER" with filename in opt.inp
+            
+			gsed -i "s/PLACEHOLDER/${confdir}/g" \
+            "conformer_files/${confdir}/${confdir}_opt.pbs" # replaces all
+            # instances of "PLACEHOLDER" with filename in opt.pbs
+		fi
+	else
+		echo "ERROR: Existing file(s) ${confdir}_opt.inp and/or" \
+        "${confdir}_opt.pbs" # overwrite protection for existing
+        # .inp or .pbs files without exiting loop/scipt
+	fi
+done
+echo Done.
+echo
+
+echo opt calculations are ready to submit to Talapas.
+echo
+
