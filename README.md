efd209ba3df759710ad98eb22c04ca1f02db67e2
Evan Christoffersen <34754998+evanchristoffersen@users.noreply.github.com>
2021-08-03 14:58:18 -0700

    Combining this repo/project with another. Soon will be dead.

--- README.md
+++
@@ -1,3 +0,0 @@
-This will be a python program, or collection of programs meant to replace these
-bash scripts for automating the electronic structure calculations done on a
-conformer library preceding the molecular dynamics simulations.
--- check_termination.py
+++
@@ -1,9 +0,0 @@
-
-def check_termination(filepath):
-    with open(filepath, 'r') as f:
-        for line in f:
-            if 'Normal termination of Gaussian ' in line:
-                return True
-    print('{} did not terminate correctly!'.format(filepath))
-    return False
-
